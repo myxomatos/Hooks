e5d4c49d37c317271a23af29e7d9e14a2bc063ec
Aldo home <myxomatos46@gmail.com>
2025-01-21 19:56:58 -0600

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './01-useState/SimpleForm'
 // import { SimpleFormWithCustomHook } from './01-useState/SimpleFormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </StrictMode>
-)
+);
