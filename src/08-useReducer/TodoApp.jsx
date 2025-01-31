97041c28dc1d850782acefa131455b846199b07c
Aldo home <myxomatos46@gmail.com>
2025-01-30 20:28:26 -0600

    Tarea TodoApp

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import React, { useReducer } from 'react';
 import { todoReducer } from './todoReducer';
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 
 const initialState = [
   {
@@ -17,6 +19,10 @@ const initialState = [
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
 
+  const handleNewTodo = (todo) => {
+    console.log({ todo });
+  };
+
   return (
     <>
       <h1>
@@ -25,32 +31,12 @@ export const TodoApp = () => {
       <hr />
       <div className='row'>
         <div className='col-7'>
-          <ul className='list-group'>
-            {todos.map((todo) => (
-              <li
-                key={todo.id}
-                className='list-group-item d-flex justify-content-between'
-              >
-                <span className='align-self-center'>Item 1</span>
-                <button className='btn btn-danger'>Borrar</button>
-              </li>
-            ))}
-          </ul>
+          <TodoList todos={todos} />
         </div>
         <div className='col-5'>
           <h4>Agregar Todo</h4>
           <hr />
-          <form>
-            <input
-              type='text'
-              placeholder='¿Que hay que hacer?'
-              className='form-control'
-            />
-
-            <button type='submit' className='btn btn-outline-primary mt-1'>
-              Agregar
-            </button>
-          </form>
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
