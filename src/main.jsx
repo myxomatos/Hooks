b412f94c145fdda97242b63a6a8ffea07b3a7fd7
Aldo home <myxomatos46@gmail.com>
2025-02-06 22:40:42 -0600

    Main, Login, Home, About, (React Router)

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,14 @@ import './index.css';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { Padre } from './07-tarea-memo/Padre';
-import { TodoApp } from './08-useReducer/TodoApp';
+// import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <TodoApp />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <MainApp />
+    </StrictMode>
+  </BrowserRouter>
 );
